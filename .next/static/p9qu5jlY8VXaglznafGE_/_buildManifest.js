88bf15b8d41ece3d5e85f182e2f1f91c1a61002a
Abrar Ranjha <Ibrar.hussain@techvention.ae>
2022-11-29 21:06:29 +0500

    updated

--- .next/static/p9qu5jlY8VXaglznafGE_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/835-7ed05418a6a14ff7.js","static/css/5674832e1468d24d.css","static/chunks/pages/index-af4c5019c7e31b9a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

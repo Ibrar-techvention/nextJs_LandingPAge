f42b57e230f6c8fafe923830f02c44903eb5fac1
Abrar Ranjha <Ibrar.hussain@techvention.ae>
2022-11-29 21:35:23 +0500

    updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3fa55e967068707"; }
+/******/ 		__webpack_require__.h = function() { return "acdcf356df00cb78"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.4d7e66cbfb358f63.hot-update.js
+++ .next/static/webpack/webpack.4d7e66cbfb358f63.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f72a4a2eeb2f4f3a"; }
+/******/ 	__webpack_require__.h = function() { return "acdcf356df00cb78"; }
 /******/ }();
 /******/ 
 /******/ }
